6fc10c05e7939ad3a6b9839aafc459dab4cd939e
Heleen Oomen <hoomen@student.42heilbronn.de>
2023-01-19 17:57:00 +0100

    ex01 complete

---
+++ cpp07/ex01/iter.h
@@ -0,0 +1,13 @@
+#ifndef ITER_H
+#define ITER_H
+
+template<typename T>
+void testTemplate(T& a) { a *= 2; }
+
+template<typename Array, typename FuncPointer>
+void iter(Array* array, int arraySize, FuncPointer functionPointer) {
+  for (int i = 0; i < arraySize; ++i)
+    functionPointer(array[i]);
+}
+
+#endif
---
+++ cpp07/ex01/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+
+#include "Layout.hpp"
+#include "iter.h"
+
+void printStars() {
+  std::cout << "\n\t   ";
+  for (int i = 0; i < 3; ++i) std::cout << Layout::emojiStar;
+  std::cout << "\n\n";
+}
+
+void printTestTimesTwo() {
+  std::cout << Layout::green << "Test " << Layout::cyanBold << "iter"
+            << Layout::green
+            << " timesTwo() function on:\n"
+               "int array[5] = {1, 3, 5, 6, 9}\n\n"
+            << Layout::brightYellow;
+}
+
+void printTestWithTemplate() {
+  std::cout << '\n';
+  std::cout << Layout::green << "Test " << Layout::cyanBold << "iter"
+            << Layout::green
+            << " instantiated template function\n"
+               "testTemplate() on:\n"
+               "float arrayF[5] = {3.2f, 5.8f, 7.0f, 18.4f, 47.33f}\n\n"
+            << Layout::brightYellow;
+}
+
+void printTestWithStringManip() {
+  std::cout << '\n';
+  std::cout << Layout::green << "Test " << Layout::cyanBold << "iter"
+            << Layout::green
+            << " strToUpper() function on:\n"
+            << "std::string s[5] = {\"Hello you!\", \"Coca-Cola\", \"Love C++\",\n"
+            << "\"Sunny days\", \"Please Go to 4aBxl16$\"}\n\n"
+            << Layout::brightYellow; 
+}
+
+void timesTwo(int& value) { value *= 2; }
+
+void strToUpper(std::string& s) {
+  std::string::iterator it;
+  for (it = s.begin(); it != s.end(); ++it) *it = std::toupper(*it);
+}
+
+void testWithNormalFunction() {
+  printTestTimesTwo();
+  int array[5] = {1, 3, 5, 6, 9};
+  iter(array, 5, &timesTwo);
+  for (int i = 0; i < 5; ++i) std::cout << '\t' << array[i] << '\n';
+}
+
+void testWithStringManip() {
+  printTestWithStringManip();
+  std::string s[5] = {
+    "Hello you!", "Coca-Cola", "Love C++", "Sunny days",
+    "Please Go to 4aBxl16$"};
+  iter(s, 5, &strToUpper);
+  std::cout << Layout::brightYellow;
+  for (int i = 0; i < 5; ++i) std::cout << '\t' << s[i] << '\n';
+}
+
+void testWithTemplate() {
+  printTestWithTemplate();
+  float arrayF[5] = {3.2f, 5.8f, 7.0f, 18.4f, 47.33f};
+  iter(arrayF, 5, &testTemplate<float>);
+  std::cout << Layout::brightYellow;
+  for (int i = 0; i < 5; ++i) std::cout << '\t' << arrayF[i] << '\n';
+}
+
+int main() {
+  printStars();
+  testWithNormalFunction();
+  testWithStringManip();
+  testWithTemplate();
+  printStars();
+}
