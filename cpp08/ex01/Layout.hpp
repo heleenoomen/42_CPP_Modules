185606a7ead04b5809539f6d0c8f0b31b548e9d6
Heleen Oomen <hoomen@student.42heilbronn.de>
2023-01-27 21:25:18 +0100

    layout as namespace

--- cpp08/ex01/Layout.hpp
+++ cpp08/ex01/Layout.hpp
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   layout.hpp                                        :+:      :+:    :+:   */
+/*   Layout.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hoomen <hoomen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -13,45 +13,40 @@
 #ifndef LAYOUT_HPP
 #define LAYOUT_HPP
 
-/* Comment the following three lines on when redirecting output to a file to
-   avoid escape codes being printed: */
+/* layout namespace for easy color printing. Define redirect_to_file if
+   recirecting output to log file in order not to print escape codes
+*/
+
 // #ifndef redirect_to_file
 // #define redirect_to_file
 // #endif
 
 namespace layout {
-#ifndef redirect_to_file
-static char const* greenBold = "\033[32;1m";
-static char const* green = "\033[32;22m";
-static char const* yellowBold = "\033[33;1m";
-static char const* yellow = "\033[33;22m";
-static char const* brightYellow = "\033[93;22m";
-static char const* redBold = "\033[31;1m";
-static char const* magentaBold = "\033[35;1m";
-static char const* magenta = "\033[35;22m";
-static char const* cyan = "\033[35;22m";
-static char const* cyanBold = "\033[36;1m";
-static char const* cyanItalic = "\033[36;3m";
-static char const* grey = "\033[0;2m";
-static char const* reset = "\033[0m";
-#else
-static char const* greenBold = "";
-static char const* green = "";
-static char const* yellowBold = "";
-static char const* yellow = "";
-static char const* brightYellow = "";
-static char const* redBold = "";
-static char const* magentaBold = "";
-static char const* magenta = "";
-static char const* cyan = "";
-static char const* cyanBold = "";
-static char const* cyanItalic = "";
-static char const* grey = "";
-static char const* reset = "";
-#endif
-static char const* emojiRobot = "\U0001F916";
-static char const* emojiSkull = "\U0001F480";
-static char const* emojiStar = "\u2B50";
-};  // namespace layout
+  extern char const* greenBold;
+  extern char const* green;
+
+  extern char const* yellowBold;
+  extern char const* yellow;
+
+  extern char const* brightYellow;
+
+  extern char const* redBold;
+
+  extern char const* magentaBold;
+  extern char const* magenta;
+
+  extern char const* cyanBold;
+  extern char const* cyanItalic;
+  extern char const* cyanItalicBold;
+  extern char const* cyan;
+
+  extern char const* grey;
+
+  extern char const* reset;
+
+  extern char const* emojiRobot;
+  extern char const* emojiSkull;
+  extern char const* emojiStar;
+}
 
 #endif
