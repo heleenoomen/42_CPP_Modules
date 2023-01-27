7a806ad3efe3a13f9874f65e736b7351617ab5e3
Heleen Oomen <hoomen@student.42heilbronn.de>
2023-01-27 19:50:16 +0100

    testing finished

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,25 +1,70 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <list>
+#include <deque>
+#include <array>
+#include <forward_list>
 
 #include "easyfind.hpp"
 
-int main() {
-  std::vector<int> v;
-  v.push_back(3);
-  v.push_back(15);
-  v.push_back(-27);
-  v.push_back(58);
+namespace testconst {
+static int const firstElem = 3;
+static int const secondElem = 15;
+static int const thirdElem = -27;
+static int const fourthElem = 38;
+};  // namespace testconst
 
+template <typename Container>
+void testFilledContainer(Container& c) {
   try {
-    std::cout << *::easyfind(v, -3) << '\n';
+    std::cout << *::easyfind(c, -3) << '\n';
   } catch (std::exception& e) {
     std::cout << e.what() << '\n';
   }
   try {
-    std::cout << *::easyfind(v, -27) << '\n';
+    std::cout << *::easyfind(c, -27) << '\n';
   } catch (std::exception& e) {
     std::cout << e.what() << '\n';
   }
+}
+
+template <typename Container>
+void fillContainer(Container& c) {
+  c.push_back(testconst::firstElem);
+  c.push_back(testconst::secondElem);
+  c.push_back(testconst::thirdElem);
+  c.push_back(testconst::fourthElem);
+}
+
+template <typename Container>
+void testContainer() {
+  Container c;
+  fillContainer(c);
+  testFilledContainer<Container>(c);
+}
+
+void testArray() {
+  std::array<int, 4> arr;
+  arr[0] = testconst::firstElem;
+  arr[1] = testconst::secondElem;
+  arr[2] = testconst::thirdElem;
+  arr[3] = testconst::fourthElem;
+  testFilledContainer(arr);
+}
+
+void testForwardList() {
+  std::forward_list<int> f;
+  f.push_front(testconst::fourthElem);
+  f.push_front(testconst::thirdElem);
+  f.push_front(testconst::secondElem);
+  f.push_front(testconst::firstElem);
+  testFilledContainer(f);
+}
+int main() {
+  testContainer<std::vector<int> >();
+  testContainer<std::list<int> >();
+  testContainer<std::deque<int> >();
+  testArray();
+  testForwardList();
   return 0;
 }
