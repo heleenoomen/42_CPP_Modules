819d400345ad7b3af01f5cbc5c2059e4fa594f3d
Heleen Oomen <hoomen@student.42heilbronn.de>
2023-01-27 21:30:14 +0100

    layout consts extern

---
+++ cpp08/ex01/layout.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Layout.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoomen <hoomen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 20:45:14 by hoomen            #+#    #+#             */
+/*   Updated: 2022/12/21 17:49:46 by hoomen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LAYOUT_HPP
+#define LAYOUT_HPP
+
+/* layout namespace for easy color printing. Define redirect_to_file if
+   recirecting output to log file in order not to print escape codes
+*/
+
+// #ifndef redirect_to_file
+// #define redirect_to_file
+// #endif
+
+namespace layout {
+  extern char const* greenBold;
+  extern char const* green;
+
+  extern char const* yellowBold;
+  extern char const* yellow;
+
+  extern char const* brightYellow;
+
+  extern char const* redBold;
+
+  extern char const* magentaBold;
+  extern char const* magenta;
+
+  extern char const* cyanBold;
+  extern char const* cyanItalic;
+  extern char const* cyanItalicBold;
+  extern char const* cyan;
+
+  extern char const* grey;
+
+  extern char const* reset;
+
+  extern char const* emojiRobot;
+  extern char const* emojiSkull;
+  extern char const* emojiStar;
+}
+
+#endif
