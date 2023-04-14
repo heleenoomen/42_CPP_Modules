eb304a85afd09570080748a23e1c8847dd571ecc
Your Name <heleen.oomen@gmail.com>
2023-04-14 16:28:34 +0200

    trailing whitespace date

--- cpp09/ex00/tools.cpp
+++ cpp09/ex00/tools.cpp
@@ -4,15 +4,18 @@
 #include <sstream>
 #include <stdexcept>
 
-// strptime() returns NULL if the string could not be parsed correctly
-// strptime() places pointer at the first byte after the date string - this
-// should be the end of string character (the zero byte)
+/* strptime() returns NULL if the string could not be parsed correctly
+strptime() places pointer at the first byte after the date string - this
+should be the end of string character (the zero byte). The zero byte may
+be preceded by trailing whitspaces */
 bool tools::isValidDate(std::string& date) {
   struct tm time;
   const char* format = "%Y-%m-%d";
   char* endOfdate = strptime(date.c_str(), format, &time);
-  if (endOfdate != NULL && !*endOfdate) return true;
-  return false;
+  if (endOfdate == NULL) return false;  // date could not be read correctly
+  while (std::isspace(*endOfdate)) endOfdate++;
+  if (!*endOfdate) return true;
+  return false;  // trailing characters other than whitespace present
 }
 
 float tools::stringToFloat(std::string& s) {
