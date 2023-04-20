13fdf6c68f6df18173e4a444d60600dffd4d73ad
Heleen Oomen <hoomen@student.42heilbronn.de>
2023-04-20 20:26:49 +0200

    comments

--- cpp09/ex02/MergeChains.hpp
+++ cpp09/ex02/MergeChains.hpp
@@ -22,9 +22,9 @@
     At the start, for every index i, AChain_[i] forms a pair with BChain_[i] so
     that AChain_[i] >= BChain_[i]
 
-    We merge the two chains by inserting elements from BChain into AChain.
+    We merge the two chains by inserting elements from BChain_ into AChain.
     We know that every B is smaller than or equal to its corresponding A, so
-    that BChain[i] should be inserted somewhere in the range AChain_[0] - AChain_[i]
+    that BChain_[i] should be inserted somewhere in the range AChain_[0] - AChain_[i]
 
     However, as we go inserting, the index where the corresponding A is, shifts 
     to the right, so that for the nth element we insert:
@@ -32,7 +32,7 @@
     We store n in the parameter AShift_.
 
     We insert in the following order:
-      * insert nth element of BChain, where n is the next number in the
+      * insert nth element of BChain_, where n is the next number in the
         Jacobsthal sequence
       * insert the skipped elements in (between the current Jacobsthal number and
       the previous Jacobsthal number) in descending order.
@@ -64,9 +64,9 @@ class MergeChains {
 
   /* Chains of integers */
   Container* AChain_;
-  Container* BChain;
+  Container* BChain_;
   
-  /* Keep track of difference in index of each B in BChain and the
+  /* Keep track of difference in index of each B in BChain_ and the
   corresponding A in AChain */
   int AShift_;
 
@@ -76,7 +76,7 @@ class MergeChains {
   /* default constructor (inaccessible) */
   MergeChains() : MergeChains<Container>(NULL, NULL) {}
 
-  /* insert the nth element of BChain in AChain, where n is the next Jacobsthal
+  /* insert the nth element of BChain_ in AChain, where n is the next Jacobsthal
   number */
   void insertNextJacobsthal() {
     prevJacobsthNb_ = JacobsthalNb_;
@@ -84,17 +84,17 @@ class MergeChains {
     insertInMainChain(JacobsthalNb_);
   }
 
-  /* insert the skipped elements in BChain, i.e. the elements n ... m, where
+  /* insert the skipped elements in BChain_, i.e. the elements n ... m, where
   n is the current Jacobsthal number and m the previous Jacobsthal number */
   void insertSkipped() {
     for (int i = JacobsthalNb_ - 1; i > prevJacobsthNb_ && nbPend_; --i)
       insertInMainChain(i);
   }
 
-  /* find the B Number for the nth element in BChain */
+  /* find the B Number for the nth element in BChain_ */
   int findNb(int elemNb) {
     int indexB = elemNb - 1;
-    return (*BChain)[indexB];
+    return (*BChain_)[indexB];
   }
 
   /* set the Range for binary search in AChain to insert the next element
@@ -110,7 +110,7 @@ class MergeChains {
   find the range where to perform binary search, perform binary search and 
   insert the number. Update the number of pending elemens & the AShift_ */
   void insertInMainChain(int elemNb) {
-    if (static_cast<size_t>(elemNb) > BChain->size()) return;
+    if (static_cast<size_t>(elemNb) > BChain_->size()) return;
     int bNb = findNb(elemNb);
     setRange(elemNb);
     iterator pos = tools::binSearch<iterator>(bNb, range_.min, range_.max);
@@ -127,12 +127,12 @@ class MergeChains {
       JacobsthalNb_(0),
       nbPend_(mainChain->size()),
       AChain_(mainChain),
-      BChain(pend),
+      BChain_(pend),
       AShift_(0) {}
 
   /* copy constructor */
   MergeChains(MergeChains const& src)
-      : AChain_(src.AChain_), BChain(src.BChain) {
+      : AChain_(src.AChain_), BChain_(src.BChain_) {
     *this = src;
   }
   /* copy assignment operator */
@@ -150,7 +150,7 @@ class MergeChains {
 
   /* public methods */
   /* as long as there are elements pending, insert first the nth number of
-  BChain where n is the next Jaconbsthal number. Then insert the skipped elements
+  BChain_ where n is the next Jaconbsthal number. Then insert the skipped elements
   between the current Jacobsthal number and the previous Jacobsthal number, so 
   that the order of insertion is: *1, *3, 2, *5, 4, *11, 10, 9, 8, etc. (As
   described at the top of this file)*/
